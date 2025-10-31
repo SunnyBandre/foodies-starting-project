9a6cd094a2bee561f8ed05e7bda6d286828c15ce
SunnyBandre <bandresunny89@gmail.com>
2025-10-31 17:24:17 +0530

    changes

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,8 +1,27 @@
-export default function MealsPage() {
+import Link from "next/link";
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
+export default async function MealsPage() {
+  const meals = await getMeals();
+
   return (
-    <div>
-      <h1>Meals Page</h1>
-      <p>Welcome to the meals section of our application!</p>
-    </div>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious Meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favourite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share Your Favourite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={meals} />
+      </main>
+    </>
   );
 }
